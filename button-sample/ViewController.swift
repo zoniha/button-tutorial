73d61a1ca168f4c286ae3581e89a928839c1e34e
zoniha <awaawapurin@gmail.com>
2021-08-04 18:26:42 +0900

    🎉 init: Initial commit

--- button-sample/ViewController.swift
+++ button-sample/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+	@IBOutlet weak var label: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
+	@IBAction func sealButton(_ sender: Any) {
+		label.text = "Seal"
+	}
 
+	@IBAction func sharkButton(_ sender: Any) {
+		label.text = "Shark"
+	}
+
+	@IBAction func squidButton(_ sender: Any) {
+		label.text = "Squid"
+	}
 }
 
